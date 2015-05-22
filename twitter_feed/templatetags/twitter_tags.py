e22aee1c56289a81ca3d4b5fdf0f97cc8235d870
Caroline Simpson <csimpson@cigionline.org>
2015-05-22 18:19:18 -0400

    Handle TypeError for python 3.4.

--- twitter_feed/templatetags/twitter_tags.py
+++ twitter_feed/templatetags/twitter_tags.py
@@ -9,7 +9,7 @@ def latest_tweets(number_of_tweets=2):
     try:
         tweets = Tweet.objects.filter(
             user__active=True).order_by('-time')[:number_of_tweets]
-    except (ValueError, AssertionError):
+    except (ValueError, AssertionError, TypeError):
         raise template.TemplateSyntaxError(
             "Tag latest_tweets requires a single positive integer argument, given %r."
             % number_of_tweets)
